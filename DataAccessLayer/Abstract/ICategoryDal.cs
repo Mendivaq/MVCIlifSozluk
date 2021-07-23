1e29b1796a79b57ca6bd61529ecdd843d16d5f29
Emirhan Coskun <coskunemirhan228@gmail.com>
2021-07-23 14:02:26 +0300

    ılif1.0

---
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    //KULLANILMAYACAK
+    public interface ICategoryDal:IRepository<Category>
+    {
+        //CRUD Buraya yazılacak 
+        //Type Name();
+        List<Category> List();
+
+        void Insert(Category p);
+        void Update(Category p);
+        void Delete(Category p);
+    }
+}
