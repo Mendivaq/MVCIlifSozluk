7ac89829861e217c37467f4f564ac6a994a946c3
Emirhan Coskun <coskunemirhan228@gmail.com>
2021-07-25 01:01:27 +0300

    25072021updt

--- IlifSozluk/Controllers/CategoryController.cs
+++ IlifSozluk/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,17 @@ namespace IlifSozluk.Controllers
 {
     public class CategoryController : Controller
     {
+        CategoryManager cm = new CategoryManager();
         // GET: Category
+        //Ornek
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult GetCategoryList()
+        {
+            var categoryValues = cm.GetAllBL();
+            return View(categoryValues);
         }
     }
 }
